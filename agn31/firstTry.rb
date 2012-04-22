b5c689e309b480a24646dc8fc342d45028fc14f7
Aaron Neyer <aaron@aaronneyer.com>
2012-04-22 00:53:34 -0400

    287

--- agn31/firstTry.rb
+++ agn31/firstTry.rb
@@ -1,5 +1,1 @@
-require 'json'
-f=open ARGV[0]
-@i={};f.each{|l|;i=l.index ':';@i[l[0..i-2]]=l[i+2..-2].downcase.gsub(/[\(\)\.;:,\/]/,'').gsub /[\?!-]/,' '}
-def u m;s=[];@i.each{|k,v|g=1;m.each{|w|;g=nil if !v[w]};s<<k if g};s;end
-while 1;print '>';STDOUT.flush;STDOUT.write JSON.dump u STDIN.gets.split;end
+require 'json';@i={};open(ARGV[0]).each{|l|;i=l.index ':';@i[l[0..i-2]]=l[i+2..-2].downcase.gsub(/[\(\)\.;:,\/]/,'').gsub /[\?!-]/,' '};def u m;s=[];@i.each{|k,v|g=1;m.each{|w|;g=nil if !v[w]};s<<k if g};s;end;while 1;print '>';STDOUT.flush;STDOUT.write JSON.dump u STDIN.gets.split;end
