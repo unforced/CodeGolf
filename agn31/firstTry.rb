52a04f60a2462b75df74abb2d84cc310f1b3797f
Aaron Neyer <aaron@aaronneyer.com>
2012-04-21 20:59:45 -0400

    I made it shorter(314c)

--- agn31/firstTry.rb
+++ agn31/firstTry.rb
@@ -1,27 +1,8 @@
 require 'json'
-f = open(ARGV[0])
-def c(t)
-t.downcase.gsub(/[\(\)\.;:,\/]/,'').gsub(/[\?!-]/,' ')
-end
+f=open ARGV[0]
+def c t;t.downcase.gsub(/[\(\)\.;:,\/]/,'').gsub /[\?!-]/,' ';end
 @i={}
-f.each do |l|
-i = l.index(':')
-@i[l[0..i-2]] = c(l[i+2..-2])
-end
-def u(m)
-s = []
-@i.each_pair do |k,v|
-g = true
-m.each do |w|
-g = false unless v[w]
-end
-s << k if g
-end
-s
-end
-while 1
-print '>'
-STDOUT.flush
-STDOUT.write JSON.generate(u(STDIN.gets.split))
-end
+f.each{|l|;i=l.index ':';@i[l[0..i-2]] = c l[i+2..-2]}
+def u m;s=[];@i.each{|k,v|g=1;m.each{|w|;g = nil unless v[w]};s<<k if g};s;end
+while 1;print '>';STDOUT.flush;STDOUT.write JSON.dump u STDIN.gets.split;end
 
