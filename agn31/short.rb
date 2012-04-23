6b28a35add13b9ee616dab83e992097351615f01
Aaron Neyer <aaron@aaronneyer.com>
2012-04-22 21:55:03 -0400

    shorter

--- agn31/short.rb
+++ agn31/short.rb
@@ -1,2 +1,3 @@
-require 'json';@i={};open($*.shift).each{|l|;i=l.split(' : ',2);@i[i[0]]=i[1].downcase.gsub(/[\(\)\.;:,\/]/,'').gsub /[\?!-]/,' '};def u m;s=[];@i.each{|k,v|g=1;m.each{|w|;g=nil if !v[w]};s<<k if g};s;end;while 1;puts '>';$>.flush;puts JSON.dump u gets.split;end
-
+require 'json'
+@i={};open($*.shift).each{|l|;i=l.split(' : ',2);@i[i[0]]=i[1].downcase.gsub(/[\(\)\.;:,\/]/,'').gsub /[\?!-]/,' '}
+while 1;puts '>';$>.flush;m=gets.split;puts JSON.dump @i.collect{|k,v|g=1;m.each{|w|g=!1 if !v[w]};k if g}.compact;end
